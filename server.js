d616c9247091d07a557d30d82671ad48d0acb719
Nuwen <gleighn@gmail.com>
2015-11-01 20:45:42 -0500

    first slide finished, xs md lg

--- server.js
+++ server.js
@@ -2,4 +2,5 @@ var connect = require('connect');
 var serveStatic = require('serve-static');
 var __dirname = 'dist/';
 
+
 connect().use(serveStatic(__dirname)).listen(8080);
--- source/js/main.js
+++ source/js/main.js
@@ -3,9 +3,55 @@ var bootstrap = require('bootstrap');
 var fullpage = require('fullpage.js');
 
 $(document).ready(function(){
+    // animate timeline
+    var moveTimeline = function(index){
+        /*var hovered = $('.timeline').find('.timeline-step:hover').length;
+        if (hovered){
+            return
+        }
+        */
+        if (index){
+            console.log('index: ' + index)
+        }
+        // if an index is supplied, hover state fired
+
+        // otherwisee loop through index
+        else{
+                var current = $('.timeline-step.done');
+                var index = current.data('timeline');
+                //reset
+                if (index == 4){
+                    var next = $("li[data-timeline='1']");
+                    $('.timeline-step.done .timeline-step-label').removeClass('active animated rotateInUpLeft');
+                    current.removeClass('done');
+                    next.addClass('done');
+                    $('.timeline-step.done .timeline-step-label').addClass('active animated rotateInUpLeft');
+                    return
+                }
+                // continue next
+                nextIndex = index + 1
+                var next = $("li[data-timeline='" + nextIndex +"']");
+                $('.timeline-step.done .timeline-step-label').removeClass('active animated rotateInUpLeft');
+                current.removeClass('done');
+                next.addClass('done');
+                $('.timeline-step.done .timeline-step-label').addClass('active animated rotateInUpLeft');
+        }
+    }
+
+    // observe hover events
+    $('.timeline-step-marker').on('hover touch', function(){
+        console.log($(this));
+    });
+
+    // 
+    // init fullpage
     $('#fullpage').fullpage({
-        navigation: true,
-        navigationTooltips: ['Hi', 'Data-driven Designs'],
-        verticalCentered: true
+        //navigation: true,
+        //navigationTooltips: ['Hi', 'Data-driven Designs'],
+        verticalCentered: true,
+        //showActiveTooltip: true,
+        afterRender: function(){
+            setInterval(moveTimeline, 4200);
+        }
     });
 });
